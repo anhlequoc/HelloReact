3c155206644fad215803ed1b88ca551e84d67e7f
Anh Le Quoc <anhlequoc88@Anhs-MacBook-Pro.local>
2018-01-26 23:12:22 +0700

    learn to lecture 14

--- public/app.jsx
+++ public/app.jsx
@@ -1,23 +1,42 @@
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
-      name: "your_name",
-      message: "your_message"
+      name: "John",
+      message: "Welcome to React course"
     };
   },
+  getInitialState: function() {
+    return {
+      name: this.props.name,
+      message: this.props.message
+    };
+  },
+  onButtonClick: function(event) {
+    event.preventDefault();
+
+    this.setState({
+      name: this.refs.name.value
+    });
+    this.refs.name.value = "";
+  },
   render: function() {
-    var name = this.props.name;
-    var message = this.props.message;
+    console.log(this.state);
+    var name = this.state.name;
+    var message = this.state.message;
     return (
       <div>
         <h1>Hellooo {name}!!!!</h1>
         <p>{message + '!!!'}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Greeter name="react" message="welcome"/>,
+  <Greeter/>,
   document.getElementById('app')
 );
